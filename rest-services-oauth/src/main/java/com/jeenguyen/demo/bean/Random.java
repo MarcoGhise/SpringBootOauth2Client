d409f83699c2d1a7afbd050a5740a3ee9b710df3
MarcoGhise <MarcoGhise@users.noreply.github.com>
2017-12-19 10:48:34 +0100

    Create Random.java

---
+++ rest-services-oauth/src/main/java/com/jeenguyen/demo/bean/Random.java
@@ -0,0 +1,14 @@
+package com.jeenguyen.demo.bean;
+
+public class Random {
+
+	private int number;
+
+	public int getNumber() {
+		return number;
+	}
+
+	public void setNumber(int number) {
+		this.number = number;
+	}
+}
