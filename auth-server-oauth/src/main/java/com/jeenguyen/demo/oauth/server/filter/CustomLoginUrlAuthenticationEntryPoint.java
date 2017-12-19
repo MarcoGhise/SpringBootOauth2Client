cc67b41b6f12964fa507e10d0b9177c88ad6836f
MarcoGhise <MarcoGhise@users.noreply.github.com>
2017-12-19 10:38:02 +0100

    Create CustomLoginUrlAuthenticationEntryPoint.java

---
+++ auth-server-oauth/src/main/java/com/jeenguyen/demo/oauth/server/filter/CustomLoginUrlAuthenticationEntryPoint.java
@@ -0,0 +1,22 @@
+package com.jeenguyen.demo.oauth.server.filter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
+
+public class CustomLoginUrlAuthenticationEntryPoint extends LoginUrlAuthenticationEntryPoint{
+
+	public CustomLoginUrlAuthenticationEntryPoint(String loginFormUrl) {
+		super(loginFormUrl);
+	}
+	
+	@Override
+	protected String determineUrlToUseForThisRequest(HttpServletRequest request, HttpServletResponse response,
+	        AuthenticationException exception) {
+	    String url = super.determineUrlToUseForThisRequest(request, response, exception);
+	    return url + "?" + request.getQueryString();
+	}
+
+}
